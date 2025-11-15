89324487e39cc0d9521ae025ceba0d3da3c2eaa7
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-11-15 15:36:32 +0200

    02. usefull-things-049-js-port-01. Fix.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
+
 const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
 const allSections = document.querySelector(".main-content");
 
-function pageTransitions() {
-    for(let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener("click", function (){
+function pageTransition() {
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener("click", function () {
             let currentBtn = document.querySelectorAll(".active-btn");
             currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
             this.className += " active-btn";
@@ -15,26 +16,44 @@ function pageTransitions() {
     allSections.addEventListener("click", (e) => {
         const id = e.target.dataset.id;
 
-        if(id) {
+        if (id) {
             sectBtns.forEach((btn) => {
                 btn.classList.remove('active');
             });
-            e.target.classList.add('active');
+            e.target.classList.add("active");
 
             sections.forEach((section) => {
-                section.classList.remove('active');
+                section.classList.remove("active");
             });
+
 
             const element = document.getElementById(id);
             element.classList.add('active');
         }
     });
 
-    const themeBtn = document.querySelector('.theme-btn');
-    themeBtn.addEventListener('click', () => {
-        let element = document.body;
-        element.classList.toggle('light-mode');
+    const themeBtn = document.querySelector(".theme-btn");
+    themeBtn.addEventListener("click", () => {
+        let el = document.body;
+
+        el.classList.toggle('mode-theme');
     });
+
 }
 
-pageTransitions();
+pageTransition();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
